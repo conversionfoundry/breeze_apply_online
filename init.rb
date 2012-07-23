17c45399bd5242073a6f446ca6ad06cf975f6a77
Isaac Freeman <isaac@leftclick.com>
2012-07-23 15:35:00 +1200

    Added Component Info hook

--- init.rb
+++ init.rb
@@ -7,3 +7,7 @@ end
 Breeze.hook :admin_menu do |menu, user|
   menu << { :name => "Enquiries", :path => "/admin/enquiries" }
 end
+
+Breeze.hook :component_info do |component_info|
+	component_info << {:name => 'Breeze Pay Online', :version => Breeze::ApplyOnline::VERSION }
+end
